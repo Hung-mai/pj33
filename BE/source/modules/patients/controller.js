44c1701151b84f35b8f313af6003ee8eb87ac554
HieuVu <hieuvubk@gmail.com>
2021-11-16 21:07:25 +0700

    init prj

---
+++ BE/source/modules/patients/controller.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const util = require('util')
+const mysql = require('mysql')
+const db = require('BE\source\models\db')
+
+module.exports = {
+    get: (req, res) => {
+    },
+    update: (req, res) => {
+    },
+    store: (req, res) => {
+        
+    },
+    delete: (req, res) => {
+        
+    }
+}
