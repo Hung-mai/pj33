46e5a4f8539e5a49e2b8f33efce0c7fb5e6c6d14
TinhPT <tinh.phantrong0612@gmail.com>
2021-11-27 01:26:14 +0700

    Sử dụng đường dẫn mặc định /home

--- BE/source/models/router.js
+++ BE/source/models/router.js
@@ -1,12 +1,11 @@
 'use strict';
 
 module.exports = function(app) {
-
+    let path = require('path');
     let api = require('./api/api');
     app.use('/api', api);
-
+    app.use('/home', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')))
     app.use((req, res) => {
-        res.redirect('/api/staff')
-        // res.status(404).send('Sorry, we cannot find that!')
+        res.redirect('/home');
     })
 };
