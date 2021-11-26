85062a565b8dedc30dcab00f0043b502fdbcd553
TinhPT <tinh.phantrong0612@gmail.com>
2021-11-27 01:28:33 +0700

    Sửa tên một số phương thức trong controller.js và định nghĩa phương thức store

--- BE/source/models/api/staffAPI.js
+++ BE/source/models/api/staffAPI.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const staffAPI = express.Router();
 const staffController = require('../../modules/staff/controller');
+const postMiddleware = require('../../middlewares/staff/post');
 
-staffAPI.get('/', staffController.getAll);
-staffAPI.post('/', staffController.store);
-staffAPI.put('/', staffController.update);
-staffAPI.delete('/', staffController.delete);
+staffAPI.get('/', staffController.getAll)
+    .get('/:id', staffController.getById)
+    .post('/', postMiddleware, staffController.store)
+    .put('/', staffController.update)
+    .delete('/', staffController.delete);
 
 module.exports = staffAPI;
--- BE/source/modules/staff/controller.js
+++ BE/source/modules/staff/controller.js
@@ -3,21 +3,35 @@
 const util = require('util')
 const mysql = require('mysql')
 const db = require('../../models/db');
+const toSQLDate = require('../../utils/toSQLDate');
 
 module.exports = {
     getAll: (req, res) => {
-        let query = `SELECT * FROM ${process.env.DB_NAME || "project3"}.hospital`;
+        let query = `SELECT * FROM ${process.env.DB_NAME || "project3"}.staff`;
         db.query(query, (error, response) => {
-            console.log(error);
-            console.log(response);
             res.send(response);
         })
     },
-    getOne: (req, res) => {
+    getById: (req, res) => {
+        let query = `SELECT * FROM ${process.env.DB_NAME || "project3"}.staff WHERE staffId = ${req.params.id}`;
+        db.query(query, (error, response) => {
+            res.send(response);
+        })
     },
     update: (req, res) => {
     },
     store: (req, res) => {
+        let query = `INSERT INTO ${process.env.DB_NAME || "project3"}.staff (hospitalId, staffName, phone, dob, address) VALUES (${req.body.hospitalId}, '${req.body.staffName}', '${req.body.phone}', '${toSQLDate(req.body.dateOfBirth)}', '${req.body.address}');`
+        db.query(query, (error, response) => {
+            if (error) {
+                res.status(400).send(error);
+            }
+            else {
+                res.status(200).send({
+                    status: "Success"
+                });
+            }
+        })
     },
     delete: (req, res) => {
     }
