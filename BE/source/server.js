d9735a4570bb20c329f343c4ec9b8048024c2f1a
TinhPT <tinh.phantrong0612@gmail.com>
2021-11-27 01:29:45 +0700

    Thêm middleware express.json() và express.urlencoded(), cho phép parse request body về dạng json

--- BE/source/server.js
+++ BE/source/server.js
@@ -2,9 +2,10 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
 
+app.use(express.json(), express.urlencoded({extended: true}));
+
 const router = require('./models/router');
 router(app);
-
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
